22cf9961e33567af90159f9e9f88f260b6fc1bd3
Prajapati Praveen <prajapatipraveen1104@gmail.com>
2024-08-16 15:18:31 +0530

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,17 @@
+#find the student from cs depaetment
+e=input("Enter Enrollment no:ex-CS24M041").upper()
+l=len(e)
+dep1=e[0:2]
+year=e[2:4]
+degree=e[4:5]
+rno=e[5:]
+if(dep1=="CS"):
+    department="Computer Science"
+    if(degree=="M"):
+        degree="MCA"
+    elif(degree=="B"):
+        degree="BCA"
+    print("student is cs department")
+    print("Department: {}   Year :20{}   Degree:{}    Roll No:{} ".format(department,year,degree,rno))
+else:
+    print("student is not cs department:")
