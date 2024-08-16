246514dbdfd91420a100926f3f6665ceddf21a74
Prajapati Praveen <prajapatipraveen1104@gmail.com>
2024-08-16 15:20:48 +0530

    Add files via upload

--- 8.py
+++ 8.py
@@ -1,38 +1,24 @@
-Program8:
-students=input("How Many Students : ")
-
-num=('0','1','2','3','4','5','6','7','8','9')
+#Find out pass-percentage of a class .
 while(1):
-    i=0
-    while(i<len(students)):
-       c=students[i]
-       if( not c in num):
-           students=input("please,enter valid student:")
-           break
-       i+=1
-    if(i==len(students)):
-        break  
-p=0
-i=1
-students=int(students)
-while(i<=students):
-    m=input("Enter {} Student Marks : ".format(i))
-    while(1):
-     t=0
-     while(t<len(m)):
-       c=m[t]
-       if( not c in num):
-           m=input("please,enter valid student marks")
-           break
-       t+=1
-       
-     if(t==len(m)):
-        break 
-    m=int(m) 
-    if(m>100 or m<0):
-        print("please,enter valid student marks")
-        continue
-    if(m>=40):
-        p+=1
-    i+=1    
-print("Percantage of Passing Students {}%".format((p*100)/students))
+    num=input("Enter Number of Student:")
+    if (num.isnumeric()):
+        break
+    else:
+        print("please Enter Number of Student valid Number...!")
+ #user input values
+num=int(num)
+count=0
+Fail=0
+for i in range(0,num):
+    m1=int(input("Enter student  marks:"))
+    if m1>=40:
+        count = count+1
+        #print(count)
+    else:
+        Fail=+1
+        
+if count>0:
+    temp=count*100/num
+    print("Passed Student:",temp,"%")
+    temp=Fail*100/num
+    print("Fail Student",Fail,"%")
