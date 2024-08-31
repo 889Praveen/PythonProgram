7b6e179bf7077516670a210c58b5d4854d168a01
Prajapati Praveen <prajapatipraveen1104@gmail.com>
2024-08-31 09:37:19 +0530

    Add files via upload

--- 12.py
+++ 12.py
@@ -2,8 +2,11 @@
 bri={'praveen':'03/08/2004',
       'vikram':'05/06/2005',
     'manish':'12/05/2006'}
-month=input("Enter brithday month(mm)")
+month=input("Enter brithday month(mm):")
 for name,bday in bri.items():
     bday_month=bday[3:5]
     if bday_month==month:
         print(name)
+    else:
+        print("Data Not Found..!");
+        break
