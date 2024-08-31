d78d12f6b3c807fe1822650616a1b9d4271090e9
Prajapati Praveen <prajapatipraveen1104@gmail.com>
2024-08-31 10:54:11 +0530

    Add files via upload

--- 12.py
+++ 12.py
@@ -1,13 +1,13 @@
 #Find brithdays in a month using dictionary
-bri={'praveen':'03/08/2004',
-      'vikram':'05/06/2005',
-    'manish':'12/12/2006',
-    'raj':'22/10/21'}
+bri={'Praveen':'03/08/2004',
+      'Vikram':'05/06/2005',
+    'Manish':'12/12/2006',
+    'Raj':'22/10/21'}
 month=input("Enter brithday month(mm):")
 count=0
 for name,bday in bri.items():
     if bday[3:5]==month:
-        print(name)
+        print(name,bday)
         count=count+1
         break
 
